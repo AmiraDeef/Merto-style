3c97bf61ca03acb4fb882df4be15bb2c7731681c
AmiraDeef <amiradeef4@gmail.com>
2026-03-14 19:03:19 +0200

    center image zoom-in

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-const bigImg=document.getElementById('img-center')
-const smallImgs=document.querySelectorAll('.img-small')
+const bigImg = document.getElementById('img-center')
+const smallImgs = document.querySelectorAll('.img-small')
+const color = document.getElementById('color')
 
-smallImgs.forEach(img=>{
-    img.addEventListener('click',function(){
-        bigImg.src=img.src
+
+$("#img-center").elevateZoom({
+    zoomType: "inner",
+    cursor: "crosshair"
+})
+smallImgs.forEach(img => {
+
+    img.addEventListener('click', function () {
+
+        bigImg.src = img.src
+       $("#img-center").data('elevateZoom')
+      .swaptheimage(img.src, img.src)
+
+        color.textContent = img.alt
+
     })
 })
+
+
+
